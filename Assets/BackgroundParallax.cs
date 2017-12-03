8207529791c0eb7c62e4350e7a9e82ba4114f29a
Racso <correo@oscargomez.net>
2017-12-03 13:59:39 -0500

    Paralax, entorno, suelo

---
+++ Assets/BackgroundParallax.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundParallax : MonoBehaviour {
+
+    [System.Serializable]
+    public class ParallaxLayer
+    {
+        public GameObject GameObject;
+        [Range(-100,100)]
+        public int MovementX;
+        [Range(-100, 100)]
+        public int MovementY;
+    }
+
+    public GameObject Camera;
+    public List<ParallaxLayer> Layers;
+
+	void Start () {
+		
+	}
+	
+	void LateUpdate ()
+    {
+        Vector3 camDiff = Camera.transform.position-transform.position;
+        foreach (var layer in Layers)
+        {
+            layer.GameObject.transform.position = transform.position - new Vector3(camDiff.x * layer.MovementX/100f, camDiff.y * layer.MovementY/100f, 0);
+        }
+	}
+}
